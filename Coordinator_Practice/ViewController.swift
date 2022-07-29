5b063df0ba7c403a242f4839763469572ae3b571
Jiseong <yim2627@gmail.com>
2022-07-29 12:38:46 +0900

    proj init

---
+++ Coordinator_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coordinator_Practice
+//
+//  Created by 임지성 on 2022/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
